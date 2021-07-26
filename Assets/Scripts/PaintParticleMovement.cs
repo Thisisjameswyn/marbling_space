25f14acb8682ccf5b8c8c5a84d699a4a69ea91b1
James Wyn <thisisjameswyn@gmail.com>
2021-07-26 16:51:00 -0700

    adding kinematic particle triggers

--- Assets/Scripts/PaintParticleMovement.cs
+++ Assets/Scripts/PaintParticleMovement.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FluidPoolDraw : MonoBehaviour
+public class PaintParticleMovement : MonoBehaviour
 {
   Camera cam;
   [SerializeField]
-  GameObject fluidParticle;
+  GameObject painterParticle;
   // Start is called before the first frame update
   void Start()
   {
@@ -16,12 +16,21 @@ public class FluidPoolDraw : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    if (Input.GetMouseButton(1))
+    if (Input.GetMouseButton(0))
     {
       Vector2 screenPos = Input.mousePosition;
       Vector2 worldPosition = cam.GetComponent<Camera>().ScreenToWorldPoint(screenPos);
 
-      Instantiate(fluidParticle, worldPosition, Quaternion.identity);
+      painterParticle.transform.position = worldPosition;
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.tag == "fluid")
+    {
+      Rigidbody2D fluidRB = other.GetComponent<Rigidbody2D>();
+      fluidRB.isKinematic = false;
     }
   }
 }
