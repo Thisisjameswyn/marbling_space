132d14709bc47cb0d699c90c74d1d5d66efe7cef
James Wyn <thisisjameswyn@gmail.com>
2021-07-28 10:37:20 -0700

    add color pallets

--- Assets/Scripts/CameraPaintController.cs
+++ Assets/Scripts/CameraPaintController.cs
@@ -9,8 +9,9 @@ public class CameraPaintController : MonoBehaviour
   GameObject paintParticle;
   Renderer paintParticleRenderer;
   Color currentColor;
+  Color[] myColors = { new Color(31, 43, 133, 255), new Color(203, 231, 250, 255), new Color(107, 32, 132, 255), new Color(167, 219, 200, 255), new Color(102, 199, 167, 255) };
 
-  Color[] myColors = { Color.black, Color.blue, Color.green, Color.red, Color.white, Color.yellow };
+  Color[,] myColorPallets = { { new Color(217, 162, 130, 255), new Color(140, 89, 77, 255), new Color(166, 166, 166, 255), new Color(64, 64, 64, 255), new Color(13, 13, 13, 255) }, { new Color(115, 48, 60, 255), new Color(16, 23, 38, 255), new Color(140, 100, 77, 255), new Color(217, 187, 169, 255), new Color(76, 99, 83, 255) }, { new Color(242, 135, 117, 255), new Color(247, 194, 137, 255), new Color(234, 228, 143, 255), new Color(249, 217, 192, 255), new Color(249, 246, 228, 255) }, { new Color(213, 94, 45, 255), new Color(216, 216, 214, 255), new Color(47, 93, 140, 255), new Color(160, 179, 193, 255), new Color(249, 249, 249, 255) }, { new Color(87, 67, 1, 255), new Color(168, 99, 38, 255), new Color(141, 147, 101, 255), new Color(217, 178, 156, 255), new Color(140, 3, 3, 255) }, { new Color(40, 27, 36, 255), new Color(60, 75, 77, 255), new Color(242, 171, 48, 255), new Color(216, 125, 13, 255), new Color(137, 10, 3, 255) }, { new Color(31, 43, 133, 255), new Color(203, 231, 250, 255), new Color(107, 32, 132, 255), new Color(167, 219, 200, 255), new Color(102, 199, 167, 255) } };
   int arrayPos = 0;
   // Start is called before the first frame update
   void Start()
@@ -18,6 +19,7 @@ public class CameraPaintController : MonoBehaviour
     cam = this.GetComponent<Camera>();
     // paintParticleRenderer = paintParticle.GetComponent<Renderer>();
     currentColor = myColors[0];
+    // myColorPallets = {{myColors},{myColors},{myColors},{myColors},{myColors},{myColors},{myColors}};
   }
 
   // Update is called once per frame
